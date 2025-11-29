eaf63df6bce28fdcdd7c72b6c640dc813ee8274c
Jon <almarezjbm@gmail.com>
2025-11-29 14:42:49 +0800

    add exchange rate

--- currentcy-converter.py
+++ currentcy-converter.py
@@ -13,7 +13,25 @@ def get_currencies():
 
     return data
 
+def print_currencies(data):
+    printer.pprint([f"{key}: {value}" for key, value in data.items()])
+
+def exchange_rate():
+    from_currency = input("Enter the currency you have (e.g., USD): ").upper()
+    to_currency = input("Enter the currency you want (e.g., EUR): ").upper()
+    amount = float(input(f"Enter the amount of {from_currency} to convert: "))
+
+    endpoint = f"v1/convert?apikey={API_KEY}&from={from_currency}&to={to_currency}&amount={amount}"
+    url = BASE_URL + endpoint
+    data = get(url).json()["data"]
+
+    rate = data["rate"]
+    converted_amount = data["result"]
+
+    print(f"{amount} {from_currency} is equal to {converted_amount} {to_currency} at a rate of {rate}.")
 
 
-data = get_currencies()
-printer.pprint(data)
+#data = get_currencies()
+#print_currencies(data)
+    
+exchange_rate()
